75a5c066a6152f4fc83a5c48d972659de491ff60
Rylen del Rio <rylendelrio@Rylens-MacBook-Pro.local>
2018-10-24 16:43:37 -0700

    test

--- models/schema.js
+++ models/schema.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const movieSchema = new mongoose.Schema({
-  title: { type: String, required: true },
+  title: { String, required: true },
   director: String,
   rating: Number,
   year: Number
@@ -10,3 +10,4 @@ const movieSchema = new mongoose.Schema({
 const Movies = mongoose.model('Movies', movieSchema);
 
 module.exports = Movies;
+// test
