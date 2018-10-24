2c6768ba4f722aa75a6b7808addc8e3f930e6625
alyssackieffer <alyssackieffer@Alyssas-MacBook-Pro.local>
2018-10-24 18:13:50 -0400

    Controllers updated

---
+++ controllers/movies.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/movies')
+mongoose.connection.once('open', ()=>{
+  console.log('Connected to Mongoose');
+});
+
+const Movies = require('../models/schema.js');
+
+//Create Route
+router.post('/', (req, res)=>{
+  Movies.create(req.body, (err, createdMovie)=>{
+    res.json(createdMovies);
+  });
+});
+
+//Index Route
+router.get('/', (req, res)=>{
+  Movies.find({}, (err, foundMovies)=>{
+    res.json(foundMovies);
+  });
+});
+
+//Delete Route
+router.delete('/:id', (req, res)=>{
+  Movies.findByIdAndRemove(req.params.id, (err, deletedMovie)=>{
+    res.json(deletedMovie);
+  });
+});
+
+//Update Route
+router.put('/:id', (req, res)=>{
+  Movies.findByIdAndUpdate(req.params.id, req.body, { new:true }, (err, updatedMovie)=>{
+    res.json(updatedMovie);
+  });
+});
+
+module.exports = router;
